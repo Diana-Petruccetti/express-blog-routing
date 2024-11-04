0b9aaf49a53ccb5cd8facf51760516cb642a641d
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-11-04 17:59:47 +0100

    new route to app

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+/* Il mio codice */
+/* Primo endpoint */
+const menu = require('./db/menu.js')
+
+app.get('/pizze', (req, res) => {
+    res.json({ data: menu, count: menu.length })
+  });
+
+/* Secondo endpoint */
+app.get('/pizze/:id', (req, res) => {
+    const pizza = menu.find((pizza) => pizza.id === parseInt(req.params.id))
+    if (!pizza) {
+    return res.status(404).json({ error: "No pizza found with that id" })
+    }
+  return res.status(200).json({ data: pizza })
+});
---
+++ db/menu.js
@@ -0,0 +1,44 @@
+const menu = [
+    {
+      id: 1,
+      name: "Margherita",
+      slug: "margherita",
+      type: "classica",
+      image: "pizze/margherita.webp",
+      ingredients: ["pomodoro", "mozzarella"],
+    },
+    {
+      id: 2,
+      name: "Marinara",
+      slug: "marinara",
+      type: "classica",
+      image: "pizze/marinara.jpeg",
+      ingredients: ["pomodoro", "aglio", "origano"],
+    },
+    {
+      id: 3,
+      name: "Diavola",
+      slug: "diavola",
+      type: "speciale",
+      image: "pizze/diavola.jpeg",
+      ingredients: ["pomodoro", "mozzarella", "salame piccante"],
+    },
+    {
+      id: 3,
+      name: "Bufalina",
+      slug: "bufalina",
+      type: "speciale",
+      image: "pizze/bufalina.jpeg",
+      ingredients: ["pomodoro", "mozzarella di bufala"],
+    },
+    {
+      id: 4,
+      name: "4 formaggi",
+      slug: "4-formaggi",
+      type: "classica",
+      image: "pizze/4_formaggi.jpeg",
+      ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
+    }
+  ]
+  
+  module.exports = menu;
